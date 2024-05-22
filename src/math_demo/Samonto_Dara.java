0793eee19cb09cdfd48cf1af8ebaa5cddc154f84
kamrul-i <komrul45@gmail.com>
2024-05-22 22:17:38 +0600

    গুণের নামতা লিখার উদাহরণ ।।

--- src/math_demo/Samonto_Dara.java
+++ src/math_demo/Samonto_Dara.java
@@ -5,25 +5,20 @@ import java.util.Scanner;
 public class Samonto_Dara {
 	public static void main(String[] args) {
 		
-		
-		
 		try (Scanner input = new Scanner(System.in)) {
-			    
-			//series_1.5 * 2.5 * 3.5 * .................*p
-			double p, into = 1;
-			System.out.print("Enter the last number : ");    
-			p = input.nextDouble();	
 			
-			for(double i=1.5; i<=p; i++){
-		           
-				System.out.println(i+" ");		           
-				into = into * i;		       
-			}		       
-			System.out.printf("\n");
-			System.out.print(p+" এর মধ্যে দশমিক সংখ্যাগুলোর গুণফল =");
-			System.out.printf("%.2f",into);         
+			    
+			//namota magic.............................................
+	        int num;  
+	        System.out.print("Enter any number : ");
+	        num = input.nextInt();
+	        
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(num + " x " + i + " = " + num * i);
+	        }	      
+	        //---------------------------------------------------------
+          
 		}
-		//---------------------------------------------------------
 			
 	}
 
