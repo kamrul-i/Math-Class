96568efb61b86ea6ed76a05e80f3c915f2d95f77
kamrul-i <komrul45@gmail.com>
2024-05-22 19:42:38 +0600

    1+3+5+.............+n তম পদ

--- src/math_demo/Samonto_Dara.java
+++ src/math_demo/Samonto_Dara.java
@@ -9,21 +9,20 @@ public class Samonto_Dara {
 		
 		try (Scanner input = new Scanner(System.in)) {
 					
-			//series_1+2+3+.............+m
-			int m, sum = 0;
-			
-			System.out.print("Enter the last number : ");
-			m = input.nextInt();
-			
-			
-			for(int i = 1; i <= m; i++){
-			    System.out.print(i+" ");
-			    sum = sum + i;
-			}
+	        //series_1+3+5+................................+n তম পদ
+	        int n, num = 0;
+	        
+	        System.out.print("Enter the last number : ");
+	        n = input.nextInt();
+	        
+	        
+	        for(int i = 1; i <= n; i=i+2){
+	            num = num + i;
+	            System.out.print(i+" ");
+	        }
 	        System.out.println();
-	        System.out.println(" ="+sum);
+	        System.out.println(" ="+num);
 		}
-         //series_1+2+3+.............+m
 		
 		
 	}
